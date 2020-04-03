dd18a88aa5b63134690036040cea59aa684b6df1
draghicistefan <draghicistefan1984@gmail.com>
2020-04-03 16:52:42 +0300

    api start

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+import requests
+import json
+
+#Publishable API Token: pk_e72e1d360c58425e8a038cb4a8409963 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    api_request=requests.get("https://cloud.iexapis.com/stable/stock/AAAU/quote?token=pk_e72e1d360c58425e8a038cb4a8409963")
+    try:
+        response=json.loads(api_request.content)
+    except:
+        response='error'
+    return render(request, 'home.html', {'response':response})
 
 
 def about(request):
