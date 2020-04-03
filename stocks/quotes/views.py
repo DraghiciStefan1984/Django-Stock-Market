387a58e22f50251b6ab9fff6938f500608e11dcb
draghicistefan <draghicistefan1984@gmail.com>
2020-04-03 18:16:18 +0300

    get data works

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -6,12 +6,16 @@ import json
 
 # Create your views here.
 def home(request):
-    api_request=requests.get("https://cloud.iexapis.com/stable/stock/AAAU/quote?token=pk_e72e1d360c58425e8a038cb4a8409963")
-    try:
-        response=json.loads(api_request.content)
-    except:
-        response='error'
-    return render(request, 'home.html', {'response':response})
+    if request.method=='POST':
+        ticker=request.POST['ticker']
+        api_request=requests.get('https://cloud.iexapis.com/stable/stock/'+ticker+'/quote?token=pk_e72e1d360c58425e8a038cb4a8409963')
+        try:
+            response=json.loads(api_request.content)
+        except Exception as e:
+            response='error'
+        return render(request, 'home.html', {'response':response})
+    else:
+        return render(request, 'home.html', {'ticker':'Enter a ticker symbol above.'})
 
 
 def about(request):
